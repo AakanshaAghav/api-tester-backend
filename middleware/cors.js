90cafa5602b059a6f7bb0dae4d81171ef48d1764
AakanshaAghav <aakanshaaghav4@gmail.com>
2025-12-05 18:25:49 +0530

    Update CORS to allow deployed frontend and local dev

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,18 @@
 const cors = require('cors');
 
+const allowedOrigins = [
+  'http://localhost:3000', // local dev
+  'https://api-tester-app-ochre.vercel.app' // deployed frontend
+];
+
 module.exports = cors({
-  origin: 'http://localhost:3000',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS not allowed'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true
 });
